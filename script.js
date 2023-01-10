a50525118c2829f3e05b024eabbee4c00d49efaa
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-01-10 19:35:33 +0100

    Check if random_num generates int between 0-2

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //create function which randomly chooses between RPS
-    //function with no parameter
+
+//function with no parameter
+function getComputerChoice(){    
     //create variable which stores random number between 0-2
+    let random_num = Math.floor(Math.random()*3);
+    console.log(random_num)
     //if 0 return Rock
     //if 1 return Paper
     //if 2 return Scissors
+}
+
+getComputerChoice()
