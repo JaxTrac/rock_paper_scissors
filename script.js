09a3cae6301989dbffafa112651f8bfd2d4eab83
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-01-10 19:27:06 +0100

    Create pseudocode for random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("test");
+//create function which randomly chooses between RPS
+    //function with no parameter
+    //create variable which stores random number between 0-2
+    //if 0 return Rock
+    //if 1 return Paper
+    //if 2 return Scissors
