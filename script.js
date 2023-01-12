753aca6b300e61002a70e25bee972f47086ace94
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-01-12 18:11:42 +0100

    Var always display "Paper", fixed

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ function getComputerChoice(){
     //create variable which stores random number between 0-2
     let random_num = Math.floor(Math.random()*3);
     //if random_num = 0 return Rock
-    if (random_num=0) {
+    if (random_num==0) {
         return ("Rock");
-    
     //else if random_num = 1 return Paper
-    } else if (random_num=1){
+    } else if (random_num==1){
         return ("Paper");
+
+
     //else if random_num = 2 return Scissors
-    } else if (random_num=2){
+    } else if (random_num==2){
         return ("Scissors");
     }
 }
