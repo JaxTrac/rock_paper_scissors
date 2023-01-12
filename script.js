5f561d21c8c50885b8a1ffd0fc39b36be576f5a6
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-01-12 18:18:48 +0100

    Pseudocode for funciton playRound

--- script.js
+++ script.js
@@ -6,14 +6,26 @@ function getComputerChoice(){
     let random_num = Math.floor(Math.random()*3);
     //if random_num = 0 return Rock
     if (random_num==0) {
-        return ("Rock");
+        return ("rock");
     //else if random_num = 1 return Paper
     } else if (random_num==1){
-        return ("Paper");
+        return ("paper");
 
 
     //else if random_num = 2 return Scissors
     } else if (random_num==2){
-        return ("Scissors");
+        return ("scissors");
     }
 }
+
+
+//function with two parameters player & computer
+    //lowercase user input
+    
+    // inputs the same -> draw
+    
+    // rock vs paper -> paper wins
+    
+    // rock vs scissors -> rock wins
+    
+    // scissors vs paper -> scissors wins
